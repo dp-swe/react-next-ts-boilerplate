d81306ec5816a09e02ff55cd1a6aea691b8bfc68
paleari <daniele.paleari@badiapp.com>
2021-02-23 14:16:36 +0100

    Add RTL config

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,7 +5,7 @@ describe('Main', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
     expect(
-      screen.getByRole('text', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
   })
